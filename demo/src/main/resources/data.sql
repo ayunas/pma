e0dadabe3a0d18d4686b7abc3108501b478648da
Amir Yunas <ayunas@gmail.com>
2020-10-22 08:56:32 -0500

    referential integrity error when seeding the database first with employees mapped to projects before projects were created. seeded project data first to overcome.

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,19 +1,21 @@
--- -- INSERT EMPLOYEES
-insert into employee (id, first_name, last_name) values (1, 'John', 'Warton');
-insert into employee (id, first_name, last_name) values (2, 'Mike', 'Lanister');
-insert into employee (id, first_name, last_name) values (3, 'Steve', 'Reeves');
-insert into employee (id, first_name, last_name) values (4, 'Ronald', 'Connor');
-insert into employee (id, first_name, last_name) values (5, 'Jim', 'Salvator');
-insert into employee (id, first_name, last_name) values (6, 'Peter', 'Henley');
-insert into employee (id, first_name, last_name) values (7, 'Richard', 'Carson');
-insert into employee (id, first_name, last_name) values (8, 'Honor', 'Miles');
-insert into employee (id, first_name, last_name) values (9, 'Tony', 'Roggers');
---
 -- -- INSERT PROJECTS
 insert into project (id, name, stage, description) values (1000, 'Large Production Deploy', 'NOTSTARTED', 'This requires all hands on deck for the final deployment of the software into production');
 insert into project (id, name, stage, description) values (1001, 'New Employee Budget',  'COMPLETED', 'Decide on a new employee bonus budget for the year and figureout who will be promoted');
 insert into project (id, name, stage, description) values (1002, 'Office Reconstruction', 'INPROGRESS', 'The office building in Monroe has been damaged due to hurricane in the region. This needs to be reconstructed');
 insert into project (id, name, stage, description) values (1003, 'Improve Intranet Security', 'INPROGRESS', 'With the recent data hack, the office security needs to be improved and proper security team needs to be hired for implementation');
+
+
+-- -- INSERT EMPLOYEES
+insert into employee (id, first_name, last_name, project_id) values (1, 'John', 'Warton', 1001);
+insert into employee (id, first_name, last_name, project_id) values (2, 'Mike', 'Lanister', 1002);
+insert into employee (id, first_name, last_name, project_id) values (3, 'Steve', 'Reeves', 1002);
+insert into employee (id, first_name, last_name, project_id) values (4, 'Ronald', 'Connor', 1000);
+insert into employee (id, first_name, last_name, project_id) values (5, 'Jim', 'Salvator', 1000);
+insert into employee (id, first_name, last_name, project_id) values (6, 'Peter', 'Henley', 1000);
+insert into employee (id, first_name, last_name, project_id) values (7, 'Richard', 'Carson', 1003);
+insert into employee (id, first_name, last_name, project_id) values (8, 'Honor', 'Miles', 1003);
+insert into employee (id, first_name, last_name, project_id) values (9, 'Tony', 'Roggers', 1003);
+--
 
 -- -- INSERT PROJECT_EMPLOYEE_RELATION (Removed duplicates from video)
 -- -- insert into project_employee (employee_id, project_id) (1,1000);
