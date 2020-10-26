2e445611f13d99f8071c343bf5cecc4f6f3bf61f
Amir Yunas <ayunas@gmail.com>
2020-10-26 12:51:58 -0500

    using demo app instead of scratch

--- scratch/src/main/java/com/jrp/pma/scratch/PMApp.java
+++ scratch/src/main/java/com/jrp/pma/scratch/PMApp.java
@@ -1,5 +1,6 @@
 package com.jrp.pma.scratch;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +9,7 @@ public class PMApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PMApp.class, args);
-		System.out.println("hello PMA");
+		System.out.println("hello PMA" + " running version : ");
 	}
 
 }
